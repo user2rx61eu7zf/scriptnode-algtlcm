c77f0fdfdc3fb7301d0b1e71a4f80d5b5d214a8c
user2rx61eu7zf <negantwd68@gmail.com>
2023-04-15 04:01:55 +0100

    changement sur le serveur

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const http = require('http')
 const main= require('./main')
 const port= process.env.PORT ||3000
 const server= http.createServer(main)
+server.on("request",(req,res)=>{
+    res.write("bonojour")
+    res.end()
+})
 server.listen(port,()=>{
     console.log("marche sur le port "+ port)
 })
