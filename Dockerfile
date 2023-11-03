3457651501f2a4b3dc656fc2d53e3f607090cf69
user2rx61eu7zf <negantwd68@gmail.com>
2023-11-03 12:00:27 +0100

    lastoflast

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/puppeteer/puppeteer:19.9.0
+FROM ghcr.io/puppeteer/puppeteer:21.5.0
 
 ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
     PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome-stable
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const http = require('http')
+const express = require("express");
 const main= require('./main')
-const port= process.env.PORT ||3000
-const server= http.createServer(main)
-server.listen(port,()=>{
-    console.log("marche sur le port "+ port)
-})
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/scrape", (req, res) => {
+    main(res);
+  });
+  
+  app.get("/", (req, res) => {
+    res.send("Render Puppeteer server is up and running!");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
